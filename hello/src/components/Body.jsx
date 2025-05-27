fd22e24e9e733db7f57f5fd5e7ccb7a9b5930ebf
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-05-26 22:19:18 -0300

    feat: inicialização do aprendizado com react

---
+++ hello/src/components/Body.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='Body'>
+            <h2 className='titulo2'>nunca mais vou ser CLT</h2>
+            <p>"Acima de todo ódio e rancor que sinto pelo mundo<br />
+            E acima de todo amor que já senti por cada ser humano vivo, eu me odeio<br />
+            Tive a existência mais fútil de todas<br />
+            E tive medo de existir com as minhas individualidades<br />
+            Mas amanhã o Sol nasce de novo, comigo aqui, ou não<br />
+            Os pássaros vão cantar de manhã e no fim do dia o Sol vai se pôr como sempre<br />
+            Então não se apeguem tanto a isso<br />
+            As coisas começam e acabam, vivem e morrem<br />
+            E existir é aceitar o início, mas aceitar ainda mais o fim"</p>
+        </div>
+    )
+}
+
+export default Body
