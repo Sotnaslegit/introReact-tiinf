1c215c6a7a7f9a687de25de78f2f71067144d48a
ERICK RODRIGUES DOS SANTOS <erick_r_dos-santos@estudante.sesisenai.org.br>
2025-05-27 22:13:47 -0300

    feat: funções iradas

--- hello/src/components/Body.jsx
+++ hello/src/components/Body.jsx
@@ -1,19 +1,71 @@
+import { useState } from 'react'
 import React from 'react'
 import './Body.css'
 
 function Body() {
+    const [cont, setCont] = useState(0)
+    const [inputValor, setInputValor] = useState('')
+    const [inputN1, setInputN1] = useState('')
+    const [inputN2, setInputN2] = useState('')
+
+    function aumentar() {
+        setCont(cont + 1)
+    }
+
+    function diminuir() {
+        if (cont > 0) {
+            setCont(cont - 1)
+        }
+    }
+
+    function adicionar() {
+        setCont(cont + Number(inputValor))
+    }
+
+    function somar() {
+        setCont(cont + (Number(inputN1) + Number(inputN2)))
+    }
+
+    function subtrair() {
+        if (cont > 0) {
+            setCont(cont + (Number(inputN1) - Number(inputN2)))
+        }
+    }
+
+    function multiplicar() {
+            setCont(cont + (Number(inputN1) * Number(inputN2)))
+    }
+
     return (
         <div className='Body'>
-            <h2 className='titulo2'>nunca mais vou ser CLT</h2>
-            <p>"Acima de todo ódio e rancor que sinto pelo mundo<br />
-            E acima de todo amor que já senti por cada ser humano vivo, eu me odeio<br />
-            Tive a existência mais fútil de todas<br />
-            E tive medo de existir com as minhas individualidades<br />
-            Mas amanhã o Sol nasce de novo, comigo aqui, ou não<br />
-            Os pássaros vão cantar de manhã e no fim do dia o Sol vai se pôr como sempre<br />
-            Então não se apeguem tanto a isso<br />
-            As coisas começam e acabam, vivem e morrem<br />
-            E existir é aceitar o início, mas aceitar ainda mais o fim"</p>
+            <h2 className='titulo2'>Quantas vezes alguém chorou pros mlk rir?</h2>
+            <div className="cont">
+                <button onClick={aumentar}>+</button>
+                <p>{cont}</p>
+                <button onClick={diminuir}>-</button>
+            </div>
+
+            <div className="form">
+                <input type="number"
+                    value={inputValor}
+                    onChange={(e) => setInputValor(e.target.value)}
+                />
+                <button onClick={adicionar}>Adicionar</button>
+            </div>
+
+            <div className="calculadora">
+                <input type="text"
+                    value={inputN1}
+                    onChange={(e) => setInputN1(e.target.value)}
+                />
+                <button onClick={somar}>+</button>
+                <button onClick={subtrair}>-</button>
+                <button onClick={multiplicar}>*</button>
+                <input type="text"
+                    value={inputN2}
+                    onChange={(e) => setInputN2(e.target.value)}
+                />
+            </div>
         </div>
     )
 }
